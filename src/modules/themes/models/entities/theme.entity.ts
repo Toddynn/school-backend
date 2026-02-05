a9e945c6276cf4dc6b38b73e68d4c469777bed7d
Vinícius Gabriel Todis <viniciusgabriez07@gmail.com>
2026-02-04 22:37:02 -0300

    Enhance Theme entity to support many-to-many relationship with Course
    - Added ManyToMany relationship between Theme and Course entities.
    - Updated Theme entity to include courses property for better association management.

--- src/modules/themes/models/entities/theme.entity.ts
+++ src/modules/themes/models/entities/theme.entity.ts
@@ -1,8 +1,15 @@
-import { Column, Entity } from 'typeorm';
+import { Column, Entity, ManyToMany } from 'typeorm';
+import { Course } from '@/modules/courses/models/entities/course.entity';
 import { TimestampedEntity } from '@/shared/entities/timestamped.entity';
 
 @Entity('themes')
 export class Theme extends TimestampedEntity {
 	@Column({ name: 'name', unique: true })
 	name: string;
+
+	@ManyToMany(
+		() => Course,
+		(course) => course.themes,
+	)
+	courses: Course[];
 }
